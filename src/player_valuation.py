a71eb1ee46d039ddfbce610bd58aa9b05112ea57
hank-butler <hank.butler@protonmail.com>
2026-01-15 09:08:11 -0700

    added attributes with baseline assumptions to constructor method, will update later after more analysis

--- src/player_valuation.py
+++ src/player_valuation.py
@@ -34,3 +34,68 @@ class PlayerAsset:
     efficiency_ratio: float = 0.0
     sharpe_ratio: float = 0.0
 
+class PlayerValuationModel:
+    """
+    Pricing theory based on bond pricing and portfolio theory
+    """
+
+    def __init__(self, risk_free_rate: float = 0.03):
+        """
+        Risk free rate assumption of 0.03 set as default value, can be overwritten when creating new instance
+        """
+
+        self.position_baselines = {
+            "QB": 35_000_000,
+            "WR": 18_000_000,
+            "RB": 8_000_000,
+            "TE": 10_000_000,
+            "OT": 18_000_000,
+            "OG": 12_000_000,
+            "C": 12_000_000,
+            "EDGE": 20_000_000,
+            "DL": 15_000_000,
+            "LB": 12_000_000,
+            "CB": 16_000_000,
+            "S": 12_000_000,
+            "K": 3_000_000,
+            "P": 2_000_000,
+            "LS": 1_500_000
+        }
+
+        self.epa_to_dollars = {
+            "QB": 2_500_000,
+            "WR": 1_800_000,
+            "RB": 1_200_000,
+            "TE": 1_500_000,
+            "OT": 1_000_000,
+            "OG": 900_000,
+            "C": 900_000,
+            "EDGE": 1_600_000,
+            "DL": 1_400_000,
+            "LB": 1_300_000,
+            "CB": 1_600_000,
+            "S": 1_400_000,
+            "K": 500_000,
+            "P": 400_000,
+            "LS": 300_000
+        }
+
+        self.peak_ages = {
+            "QB": 28,
+            "WR": 26,
+            "RB": 25,
+            "TE": 27,
+            "OT": 28,
+            "OG": 28,
+            "C": 29,
+            "EDGE": 27,
+            "DL": 27,
+            "LB": 27,
+            "CB": 27,
+            "S": 27,
+            "K": 30,
+            "P": 30,
+            "LS": 30
+        }
+
+    
