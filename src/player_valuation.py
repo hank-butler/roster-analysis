5ecf8b49b5e9c5c13b64e4a94aa1a8163f05bed2
hank-butler <hank.butler@protonmail.com>
2026-01-16 10:39:11 -0700

    risk score function

--- src/player_valuation.py
+++ src/player_valuation.py
@@ -98,4 +98,106 @@ class PlayerValuationModel:
             "LS": 30
         }
 
+    def calculate_expected_value(self, player: PlayerAsset) -> float:
+        """
+        Calculates expected performance value using assumptions established above
+
+        =========
+        ARGUMENTS:
+        =========
+        PlayerAsset: The player being valued
+
+        =======
+        RETURNS:
+        =======
+        float: The calculated expected value for the player
+
+        ======
+        RAISES:
+        ======
+        TypeError
+        """
+
+        base_value = self.position_baselines.get(player.position, 10_000_000)
+
+        epa_value = player.epa_total * self.epa_to_dollars.get(player.position, 1_000_000)
+
+        expected_plays = 1_000 # approximate number of plays in a 17-game season
+
+        snap_factor = min(player.snaps_played / expected_plays, 1.5) # capping at 150%
+
+        # calculating raw performance value
+        performance_value = base_value + epa_value
+        adjusted_value = performance_value * snap_factor
+
+        return max(adjusted_value, 0) # ensures number won't be negative
     
+def calculate_risk_score(self, player: PlayerAsset) -> float:
+    """
+    Quantifying risk based on age, injury history, and position.
+
+    Simplifying assumptions made, can be adjusted.
+
+    =========
+    ARGUMENTS
+    =========
+    player: PlayerAsset, the player getting the risk calculation
+
+    =======
+    RETURNS
+    =======
+    float: player's risk score
+
+    ======
+    RAISES
+    ======
+
+    
+    """
+    risk_components = []
+
+    injury_risk = min(player.games_missed / 51, 0.5) # 51 for 3 full regular seasons
+    risk_components.append(injury_risk)
+
+    peak_age = self.peak_ages.get(player.position, 27) # simplifying assumption of player peak at 27 years due to avg NFL career being ~5 seasons
+    age_diff = player.age - peak_age
+
+    if age_diff <= 0:
+        age_risk = 0.0
+    elif age_diff <= 2:
+        age_risk = 0.1
+    elif age_diff <= 4:
+        age_risk = 0.3
+    else:
+        age_risk = 0.5
+
+    risk_components.append(age_risk)
+
+    position_risk = {
+        "QB": 0.1,
+        "WR": 0.2,
+        "RB": 0.4,
+        "TE": 0.2,
+        "OT": 0.15,
+        "OG": 0.15,
+        "C": 0.15,
+        "EDGE": 0.25,
+        "DL": 0.25,
+        "LB": 0.2,
+        "CB": 0.2,
+        "S": 0.2,
+        "K": 0.1,
+        "P": 0.1,
+        "LS": 0.1
+    }
+
+    risk_components.append(position_risk)
+
+    # combined risk scores 
+    total_risk = (
+        0.4*risk_components[0] +
+        0.4*risk_components[1] +
+        0.2*risk_components[2]
+    )
+
+    return total_risk
