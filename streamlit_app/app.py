73f1b1845b4453848cdfe7afd2111a65febd097c
hank-butler <hank.butler@protonmail.com>
2026-06-15 21:17:36 -0600

    fix: add sys.path project root to all Streamlit pages

--- streamlit_app/app.py
+++ streamlit_app/app.py
@@ -1,3 +1,7 @@
+import sys
+from pathlib import Path
+sys.path.insert(0, str(Path(__file__).parent.parent))  # project root
+
 from typing import List
 
 import streamlit as st
--- streamlit_app/pages/04_sb_template.py
+++ streamlit_app/pages/04_sb_template.py
@@ -1,3 +1,7 @@
+import sys
+from pathlib import Path
+sys.path.insert(0, str(Path(__file__).parent.parent.parent))  # project root
+
 from typing import Dict, List
 
 import pandas as pd
